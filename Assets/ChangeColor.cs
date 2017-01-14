4b9db032375849458bdf68d421fee397b8c03f3d
MarieLouB <marielou.boisaubert@gmail.com>
2017-01-14 16:15:06 +0100

    Init project

---
+++ Assets/ChangeColor.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeColor : MonoBehaviour {
+	public int minDistance = 10;
+	public Color idleColor = Color.white;
+	public Color closeColor = Color.blue;
+
+	private GameObject player;
+	private ParticleSystem ps;
+	private ParticleSystem.Particle[] particles;
+
+	void Start () {
+		player = GameObject.Find("Player");
+		ps = GetComponent<ParticleSystem>();
+		particles = new ParticleSystem.Particle[ps.maxParticles];
+	}
+
+	private void LateUpdate() {
+		int numParticlesAlive = ps.GetParticles(particles);
+
+		for (int i = 0; i < numParticlesAlive; i++) {
+			if (Vector3.Distance(particles[i].position, player.transform.position) < minDistance) {
+				Debug.Log ("Bonjour");
+				particles[i].startColor = closeColor;
+			}
+			else {
+				particles[i].startColor = idleColor;
+			}
+		}
+
+		ps.SetParticles(particles, numParticlesAlive);
+	}
+}
---
+++ Assets/changeCamera.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class changeCamera : MonoBehaviour
+{
+
+    public Camera camera;
+    public Camera camera2;
+    public Camera camera3;
+
+    void Start()
+    {
+
+        camera.enabled = true;
+        camera2.enabled = false;
+        camera3.enabled = false;
+    }
+
+    void Update()
+    {
+        //This will toggle the enabled state of the two cameras between true and false each time
+        if (Input.GetKeyUp(KeyCode.C))
+        {
+            camera.enabled = !camera.enabled;
+            camera2.enabled = !camera2.enabled;
+        }
+        if (Input.GetKeyUp(KeyCode.V))
+        {
+            camera.enabled = !camera.enabled;
+            camera3.enabled = !camera3.enabled;
+        }
+    }
+}
